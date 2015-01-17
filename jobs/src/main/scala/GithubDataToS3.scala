e0f289abcc18cd8387c5a8d8ea1e594fa78d2f4f
Daniel Trinh <dtrinh@brightroll.com>
2015-01-17 11:26:07 -0800

    spark requires a main method instead of an App

--- jobs/src/main/scala/GithubDataToS3.scala
+++ jobs/src/main/scala/GithubDataToS3.scala
@@ -7,12 +7,14 @@ import org.apache.spark.SparkContext._
 import org.apache.spark.SparkConf
 import org.joda.time.DateTime
 
-class GithubDataToS3 extends App {
-  val sc = new SparkContext(new SparkConf().setAppName("Github Data To S3"))
-  val dayToMove = args(0)
-  val time = DateTime.parse(dayToMove)
+class GithubDataToS3 {
+  def main(args: Array[String]): Unit = {
+    val sc = new SparkContext(new SparkConf().setAppName("Github Data To S3"))
+    val dayToMove = args(0)
+    val time = DateTime.parse(dayToMove)
 
-  val hdfsPath = s"/${time.toString("yyyy-MM-dd")}/"
-  val githubData = sc.binaryFiles(hdfsPath)
-  githubData.saveAsHadoopFile(s"s3://github-archive-data$hdfsPath")
+    val hdfsPath = s"/${time.toString("yyyy-MM-dd")}/"
+    val githubData = sc.binaryFiles(hdfsPath)
+    githubData.saveAsHadoopFile(s"s3://github-archive-data$hdfsPath")
+  }
 }
